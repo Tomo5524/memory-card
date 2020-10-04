36e8ae77d4729a993b4915b2f9a6cf1b4c41d609
Tomo5524 <ttoommoosrst@gmail.com>
2020-10-04 21:43:35 +0900

    Updates

---
+++ precache-manifest.87ba70a24a53f821837102b581029e73.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46eddc7efbd31b6781963420afd2b4ef",
+    "url": "/memory-card/index.html"
+  },
+  {
+    "revision": "1052956ccb6da8b26370",
+    "url": "/memory-card/static/css/main.37e7c88d.chunk.css"
+  },
+  {
+    "revision": "2df190e1a640f457c425",
+    "url": "/memory-card/static/js/2.16abd75c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-card/static/js/2.16abd75c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1052956ccb6da8b26370",
+    "url": "/memory-card/static/js/main.4b0ecc14.chunk.js"
+  },
+  {
+    "revision": "2d10a56d9bedc8f13787",
+    "url": "/memory-card/static/js/runtime-main.4cf988ce.js"
+  },
+  {
+    "revision": "664397dc412fb031725ff121153d02b9",
+    "url": "/memory-card/static/media/angular.664397dc.svg"
+  },
+  {
+    "revision": "d680b4570bd8511a42eef346d818e1e2",
+    "url": "/memory-card/static/media/js.d680b457.svg"
+  },
+  {
+    "revision": "7d9fccaffda0db8340fe3e803fee9c83",
+    "url": "/memory-card/static/media/python-logo-generic.7d9fccaf.svg"
+  },
+  {
+    "revision": "1b7f7aac77d71ef59e4c9e8d2cb2067e",
+    "url": "/memory-card/static/media/react.1b7f7aac.svg"
+  },
+  {
+    "revision": "4a16cd4e21cff053a732796b4c895186",
+    "url": "/memory-card/static/media/vue.4a16cd4e.svg"
+  }
+]);
